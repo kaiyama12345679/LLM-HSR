c05f5f3b12ea78ae2ef0b27ccb3894670555f482
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2024-05-22 19:39:32 +0900

    remove files and use SQL

--- recommend.py
+++ recommend.py
@@ -4,6 +4,8 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAIEmbeddings
 import torch
 from dotenv import load_dotenv
+import sqlite3
+import uuid
 load_dotenv()
 
 template = '''Answer the following questions as best you can. You have access to the following tools:
@@ -32,21 +34,33 @@ tools = ["google-search"]
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 tools = load_tools(tools, llm=llm)
 agent = create_react_agent(llm, tools, prompt)
-agent_executor =AgentExecutor(agent=agent, tools=tools)
+agent_executor =AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-def get_content(title):
+def get_content(title: str, cur: sqlite3.Cursor):
     book_template = """
     Please Describe the content of the book "{title}. You should provides as much specific information as possible about what the book covers, important, themes, genre, proper nouns, and a summary of the content".
     Do not include any information unrelated to the book's content.
     """
     book_prompt = PromptTemplate(template=book_template, input_variables={"title"})
     book_prompt = book_prompt.format(title=title)
-    return agent_executor.invoke({"input": book_prompt})
+    if not check_book_exists(title, cur):
+        result = agent_executor.invoke({"input": book_prompt})
+        cur.execute("INSERT INTO books (id, title, content) VALUES (?, ?, ?)", (str(uuid.uuid4()), title, result["output"]))
+        return {"title": title, "output": result["output"]}
+    else:
+        cur.execute("SELECT content FROM books WHERE title=?", (title,))
+        result = cur.fetchone()
+        return {"title": title, "output": result[0]}
+    
+def check_book_exists(title, cur: sqlite3.Cursor):
+    cur.execute("SELECT * FROM books WHERE title=?", (title,))
+    result = cur.fetchone()
+    return result
 
-def get_books_content(books):
+def get_books_content(books, cur: sqlite3.Cursor):
     tasks = []
     for title in books:
-        tasks.append(get_content(title))
+        tasks.append(get_content(title, cur))
     return tasks
 
 if __name__ == "__main__":
@@ -54,13 +68,19 @@ if __name__ == "__main__":
     user_preference = input("読みたい本についての要求：")
     if type(user_preference) != str:
         raise ValueError("Please enter a valid string")
-
-    results = get_books_content(books)
-    with open("results.txt", "w") as f:
-        for result in results:
-            f.write(result["output"] + "\n")
+    try:
+        db = sqlite3.connect("./books.db")
+        cursor = db.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books (id TEXT PRIMARY KEY, title TEXT, content TEXT)")
+        db.commit()
+        results = get_books_content(books, cursor)
+        db.commit()
+    finally:
+        db.rollback()
+        db.close()
     embeddings = OpenAIEmbeddings()
     query_result = embeddings.embed_documents([result["output"] for result in results])
+    idx2title = {idx: result["title"] for idx, result in enumerate(results)}
 
     book_data = torch.tensor(query_result)
     message = [
@@ -74,5 +94,5 @@ if __name__ == "__main__":
     similarity = torch.nn.functional.cosine_similarity(book_data, pfr, dim=1)
 
     max_index = torch.argmax(similarity)
-    print(books[max_index.item()])
+    print(f"最もマッチした本は{idx2title[max_index.item()]}です。")
 
